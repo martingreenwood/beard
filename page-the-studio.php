5c5bfc3aba2ea8434f1c8ce2db7cdde74df40e82
1 <martin@pixelpudu.com>
2018-01-16 16:58:17 +0000

    fix

--- page-the-studio.php
+++ page-the-studio.php
@@ -14,21 +14,6 @@
 
 get_header(); ?>
 
-	<?php 
-	$featureimage = wp_get_attachment_url( get_post_thumbnail_id($post->ID));
-	?>
-	<!-- <section id="feature-image" class="parallax-window" data-bleed="50" data-parallax="scroll" data-image-src="<?php echo $featureimage; ?>">
-		<div class="caption table">
-			<div class="cell bottom">
-				<div class="wrapper">
-					<h1><?php the_title(); ?></h1>
-					<hr>
-					<h2><?php the_field( 'sub_heading' ); ?></h2>
-				</div>
-			</div>
-		</div>
-	</section> -->
-
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main container narrow" role="main">
 
@@ -111,23 +96,6 @@ get_header(); ?>
 	</div>
 	<?php endif; ?>
 
-	<!-- <div id="team_content" class="content-area">
-		<main id="main" class="site-main container narrow" role="main">
-
-			<article>
-
-				<div class="entry-content">
-				<?php
-					echo get_field( 'team_content' );
-				?>
-				</div>
-
-			</article>
-
-
-		</main>
-	</div> -->
-
 	<div id="team">
 		<div class="wrapper">
 		<?php $i = 1; $team = new WP_Query( array( 'post_type' => 'team', 'posts_per_page' => -1 ) ); ?>
@@ -147,25 +115,25 @@ get_header(); ?>
 							<h3><?php the_field( 'sub_heading' ); ?></h3>
 							<ul>
 								<?php if (get_field( 'instagram' )): ?>
-									<li><a href="<?php the_field( 'instagram' ); ?>" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+									<li><a href="<?php the_field( 'instagram' ); ?>" target="_blank"><i class="fab fa-instagram" aria-hidden="true"></i></a></li>
 								<?php endif; ?>
 								<?php if (get_field( 'facebook' )): ?>
-									<li><a href="<?php the_field( 'facebook' ); ?>" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+									<li><a href="<?php the_field( 'facebook' ); ?>" target="_blank"><i class="fab fa-facebook-f" aria-hidden="true"></i></a></li>
 								<?php endif; ?>
 								<?php if (get_field( 'twitter' )): ?>
-									<li><a href="<?php the_field( 'twitter' ); ?>" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+									<li><a href="<?php the_field( 'twitter' ); ?>" target="_blank"><i class="fab fa-twitter" aria-hidden="true"></i></a></li>
 								<?php endif; ?>
 								<?php if (get_field( 'linkedin' )): ?>
-									<li><a href="<?php the_field( 'linkedin' ); ?>" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+									<li><a href="<?php the_field( 'linkedin' ); ?>" target="_blank"><i class="fab fa-linkedin-in" aria-hidden="true"></i></a></li>
 								<?php endif; ?>
 								<?php if (get_field( 'google-plus' )): ?>
-									<li><a href="<?php the_field( 'google-plus' ); ?>" target="_blank"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+									<li><a href="<?php the_field( 'google-plus' ); ?>" target="_blank"><i class="fab fa-google-plus-g" aria-hidden="true"></i></a></li>
 								<?php endif; ?>
 								<?php if (get_field( 'tumblr' )): ?>
-									<li><a href="<?php the_field( 'tumblr' ); ?>" target="_blank"><i class="fa fa-tumblr" aria-hidden="true"></i></a></li>
+									<li><a href="<?php the_field( 'tumblr' ); ?>" target="_blank"><i class="fab fa-tumblr" aria-hidden="true"></i></a></li>
 								<?php endif; ?>
 								<?php if (get_field( 'behance' )): ?>
-									<li><a href="<?php the_field( 'behance' ); ?>" target="_blank"><i class="fa fa-behance" aria-hidden="true"></i></a></li>
+									<li><a href="<?php the_field( 'behance' ); ?>" target="_blank"><i class="fab fa-behance" aria-hidden="true"></i></a></li>
 								<?php endif; ?>
 							</ul>
 
